af78d0082c68cbab0c3adbe02a36d4cb2daafe65
anhlt <42168740+BryantShepherd@users.noreply.github.com>
2020-10-20 21:48:00 +0700

    refactor: remove home_tab
    feat: add popup menu

---
+++ lib/ui/home/widgets/home_tab_message.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeTabMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
+
+class _Conversation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
