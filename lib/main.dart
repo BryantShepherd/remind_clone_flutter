0936f8659682de229e206c9412ef028fb5b8a574
Hakuno <hakuno2000@gmail.com>
2020-11-29 20:19:06 +0700

    build user profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remind_clone_flutter/routes.dart';
+import 'package:remind_clone_flutter/ui/home/widgets/people_user_info.dart';
+import 'package:remind_clone_flutter/ui/home/widgets/people_user_info_test.dart';
 
 void main() {
   runApp(RemindClone());
@@ -10,10 +12,11 @@ class RemindClone extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Remind Clone",
-      home: Center(
-        child: Text("Hello World"),
-      ),
-      routes: Routes.routes,
+      // home: Center(
+      //   child: Text("Hello World"),
+      // ),
+      // routes: Routes.routes,
+      home: ProfileUserInfo(),
     );
   }
 }
