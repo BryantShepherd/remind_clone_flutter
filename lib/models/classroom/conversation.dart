74ae85462f9467f4f35b22ed6e9a47d0a7adc172
anhlt <42168740+BryantShepherd@users.noreply.github.com>
2020-12-05 11:47:25 +0700

    add conversation and msg models

---
+++ lib/models/classroom/conversation.dart
@@ -0,0 +1,62 @@
+import '../user/user.dart';
+
+class Conversation {
+  int id;
+  String name;
+  String type;
+  List<User> members;
+  List<Message> messages;
+
+  Conversation({this.id, this.name, this.type});
+
+  void setMembers(List<User> members) {
+    this.members = members;
+  }
+
+  void addMember(User member) {
+    if (members == null) {
+      throw NullThrownError();
+    }
+    this.members.add(member);
+  }
+
+  void setMessages(List<Message> messages) {
+    this.messages = messages;
+  }
+
+  void addMessage(Message message) {
+    if (messages == null) {
+      throw NullThrownError();
+    }
+    this.messages.add(message);
+  }
+
+  factory Conversation.fromJson(Map<String, dynamic> json) {
+    return Conversation(
+      id: json["id"],
+      name: json["name"] ?? json["conversation_name"],
+      type: json["type"],
+    );
+  }
+}
+
+class Message {
+  int id;
+  User sender;
+  String messageText;
+  String message;
+  String createdAt;
+
+  Message(
+      {this.id, this.messageText, this.sender, this.message, this.createdAt});
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      id: json["id"],
+      sender: User.fromJson(json["sender"]),
+      message: json["message"],
+      messageText: json["messageText"] ?? json["message_text"],
+      createdAt: json["createdAt"] ?? json["created_at"],
+    );
+  }
+}
